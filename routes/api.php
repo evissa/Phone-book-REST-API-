ecf8afd42b1ef885d41ea582a1dd95151d153972
evissa <cankaj17@live.com>
2020-12-06 21:25:03 +0100

    Documentation

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// Get access a resource and retrieve data and we don't have to modify or alter the state of this data
 Route::get('/contacts', 'App\Http\Controllers\ContactsController@index');
 Route::get('/contacts/{contact}', 'App\Http\Controllers\ContactsController@show');
+
+// Post sends data to the server
 Route::post('/contacts', 'App\Http\Controllers\ContactsController@store');
+
+// Put replace the state of some data that already exists on the system
 Route::put('/contacts/{contact}', 'App\Http\Controllers\ContactsController@update');
+
+// Delete, deletes a resource (relative to the URI we have sent) on the system
 Route::delete('/contacts/{contact}', 'App\Http\Controllers\ContactsController@destroy');
